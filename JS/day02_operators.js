abe9da9a987c3ae7833c37029fb7e8290a338e4f
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-10-17 13:43:48 +0530

    added operators

---
+++ JS/day02_operators.js
@@ -0,0 +1,40 @@
+//Operators
+var a = 10;
+var b = 20;
+
+//arithmetic operators
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+console.log(++a);
+console.log(a--);
+console.log(a);
+console.log(a++);
+console.log(a);
+
+
+//comparison operators
+console.log(a == b);
+console.log(a != b);
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+
+
+//logical operators
+console.log(a > 5 && b > 5);
+console.log(a > 5 || b > 5);
+console.log(!(a > 5 && b > 5));
+
+//ternary operator
+console.log(a > 5 ? "a is greater than 5" : "a is less than 5");
+
+//assignment operators
+console.log(a += b);
+console.log(a -= b);
+console.log(a *= b);
+console.log(a /= b);
+console.log(a %= b);
