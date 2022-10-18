9f450ec18e46d302f8912b6be5a9bdb40a2eb9a3
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-10-18 13:24:23 +0530

    added conditional statements

---
+++ JS/day03_conditionalstmts.js
@@ -0,0 +1,50 @@
+//conditional Statements
+
+
+//if
+var a = 10;
+var b = 20;
+if (a > b) {
+      console.log("a is greater than b");
+      }
+
+
+//if else
+var a = 10;
+var b = 20;
+if (a > b) {
+      console.log("a is greater than b");
+      }
+else {
+      console.log("b is greater than a");
+      }
+
+
+//if else if
+var a = 10;
+var b = 20;
+if (a > b) {
+      console.log("a is greater than b");
+      }
+else if (a < b) {
+      console.log("b is greater than a");
+      }
+else {
+      console.log("a and b are equal");
+      }
+
+      
+//switch
+var a = 10;
+switch (a) {
+      case 10:
+            console.log("a is 10");
+            break;
+      case 20:
+            console.log("a is 20");
+            break;
+      default:
+            console.log("a is not 10 or 20");
+            break;
+      }
+//Path: DevOps\JS\day04_loops.js
