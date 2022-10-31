f83e065891d234a46d399f49bb30328b4e457c56
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-10-31 13:02:33 +0530

    added a folder for programming questions

---
+++ Programming_Questions/toknow.py
@@ -0,0 +1,19 @@
+# in for loop
+'''if stp value -> +ve the check start < end
+if stp value -> -ve then check start > end'''
+
+#eg:
+for i in range(0,10,-1):
+      print(i)#no output
+
+#same in while
+i = 0
+while i>10:
+      print(i)
+      i -=1 #no output
+
+'''i = 0
+while i<10:
+      print(i)
+      i-=1 #infinite times'''
+
