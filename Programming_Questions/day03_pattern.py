8ab7755844e888a2f27aab55f2ec99778203de9d
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-11-07 12:10:32 +0530

    added pattern programming

--- Programming_Questions/day03_pattern.py
+++ Programming_Questions/day03_pattern.py
@@ -1,5 +1,5 @@
 #print based * on the rows based on given input
-n = int(input())
+'''n = int(input())
 for i in range(n):
       print("*")
 
@@ -45,3 +45,80 @@ for i in range(n):
             if val > ord("Z"):
                   val = ord("A")
       print()
+
+
+#print repeated alphabets and change alphabets for every row
+n = int(input())
+val =ord("A")
+for i in range(n):
+      for i in range(n):
+            print(chr(val),end=" ")
+      val +=1
+      if val > ord("Z"):
+                  val = ord("A")
+      print()
+
+
+#change values for every column and print same o/p for every row
+n = int(input())
+for i in range(n):
+      val =ord("A")
+      for i in range(n):
+            print(chr(val),end=" ")
+            val +=1
+            if val > ord("Z"):
+                  val = ord("A")
+      print()
+
+
+#
+n = int(input())
+res = n
+while res>=26:
+      res-=26
+val =ord("A")+ (res-1)
+for i in range(n):
+      for i in range(n):
+            print(chr(val),end=" ")
+            val +=1
+            if val > ord("Z"):
+                  val = ord("A")
+      print()
+
+
+#or
+n = int(input())
+val =ord("A")+ (n-1)
+while val>=ord("Z"):
+      val-=26
+for i in range(n):
+      for i in range(n):
+            print(chr(val),end=" ")
+            val +=1
+            if val > ord("Z"):
+                  val = ord("A")
+      print()
+
+#print * for odd and @ for even values
+n = int(input())
+symbol =["*","@"]
+for i in range(n):
+      for j in range(n):
+            print(symbol[j%2],end=" ")
+      print()
+
+#or
+n = int(input())
+for i in range(n):
+      for j in range(n):
+            print("*" if j%2==0 else "@",end=" ")
+      print()
+
+#for every rows now
+n = int(input())
+for i in range(n):
+      for j in range(n):
+            print("*" if i%2==0 else "@",end=" ")
+      print()'''
+
+
