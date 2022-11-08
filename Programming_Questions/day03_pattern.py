3ea7a70c70a5f6ca5162434a757cc0ffbcce0c4a
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-11-08 12:06:29 +0530

    added pattern programs

--- Programming_Questions/day03_pattern.py
+++ Programming_Questions/day03_pattern.py
@@ -1,5 +1,5 @@
 #print based * on the rows based on given input
-'''n = int(input())
+n = int(input())
 for i in range(n):
       print("*")
 
@@ -99,6 +99,7 @@ for i in range(n):
                   val = ord("A")
       print()
 
+
 #print * for odd and @ for even values
 n = int(input())
 symbol =["*","@"]
@@ -107,6 +108,7 @@ for i in range(n):
             print(symbol[j%2],end=" ")
       print()
 
+
 #or
 n = int(input())
 for i in range(n):
@@ -114,11 +116,71 @@ for i in range(n):
             print("*" if j%2==0 else "@",end=" ")
       print()
 
+
 #for every rows now
 n = int(input())
 for i in range(n):
       for j in range(n):
             print("*" if i%2==0 else "@",end=" ")
-      print()'''
+      print()
 
 
+#mix of caps and small letters
+n = int(input())
+val = ord("A")
+for i in range(n):
+      for j in range(n):
+            if val%2==0:
+                  newval=val+ 32
+                  print(chr(newval),end=" ")
+            else:
+                  print(chr(val),end=" ")
+            val +=1
+            if val > ord("Z"):
+                  val = ord("A")
+      print()
+
+#or
+n = int(input())
+val = ord("A")
+for i in range(n):
+      for j in range(n):
+            print(chr(val) if j%2==0 else chr(val+32),end=" ")
+            val +=1
+            if val > ord("Z"):
+                  val = ord("A")
+      print()
+
+
+n = int(input())
+l1 = ["*","@"]
+val =0
+for i in range(1,n+1):
+      for j in range(1,n+1):
+            print(l1[val],end=" ")
+            val+=1
+            if val>1:
+                  val=0
+      print()
+
+
+#or
+n = int(input())
+White=True
+for i in range(1,n+1):
+      for j in range(1,n+1):
+            print("*"if White==True else "@", end=" ")
+            White = not White
+      print()
+
+
+#print values for every columns
+n = int(input())
+val =1
+for i in range(n):
+      for j in range(n):
+            print(val, end=" ")
+            val+=1
+            if val >9:
+                  val =1
+      print()
