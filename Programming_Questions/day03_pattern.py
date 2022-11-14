b57b6dfddbb673aaff855e7f3a6f71f8d95d8258
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-11-14 12:15:34 +0530

    added pattern

--- Programming_Questions/day03_pattern.py
+++ Programming_Questions/day03_pattern.py
@@ -497,3 +497,14 @@ for i in range(n):
       print(" ",end=" ")
   print()
 
+#or
+n = int(input())
+val =ord("A")
+a = ord("A")
+for i in range(n):
+  for j in range(n):
+    if i==0 or i==n-1 or j==0 or j==n-1 or i+j==n-1:
+      print("*", end=" ")
+    else:
+      print(" ",end=" ")
+  print()
