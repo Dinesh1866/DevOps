4923d9c622d31cd2d83873df5d3f159e9414eb10
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-11-16 08:03:59 +0530

    added pattern

--- Programming_Questions/day03_pattern.py
+++ Programming_Questions/day03_pattern.py
@@ -613,3 +613,17 @@ J I H G F
 K L M N O
 T S R Q P
 U V W X Y'''
+n = int(input())
+val = ord("A")
+for i in range(1,n+1):
+  for j in range(n):
+    if i%2==1:
+      print(chr(val),end=" ")
+      val+=1
+    else:
+      print(chr(val),end=" ")
+      if j==n-1:
+        val+=1
+      else:val-=1
+  val+=(n-1)
+  print()
