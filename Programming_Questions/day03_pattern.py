353588cb8aa11b4aa9ce1c6cc746f56333abeb6c
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-12-01 11:08:40 +0530

    added pattern

--- Programming_Questions/day03_pattern.py
+++ Programming_Questions/day03_pattern.py
@@ -1226,6 +1226,18 @@ for i in range(1,n+1):
 print("\n".join(l1+l1[-2::-1]))
 
 
+#without list
+n =int(input())
+for i in range(n):
+  for j in range(i+1):
+    print("*",end=" ")
+  print()
+for i in range(n-1):
+  for j in range(n-i-1):
+    print("*",end=" ")
+  print()
+
+
 '''
 *
 * *
