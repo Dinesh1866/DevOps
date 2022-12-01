1d2bac8ba6b9d787159a9c438ded0466c493a4f2
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-12-01 11:23:16 +0530

    added pattern

--- Programming_Questions/day03_pattern.py
+++ Programming_Questions/day03_pattern.py
@@ -1238,6 +1238,15 @@ for i in range(n-1):
   print()
 
 
+#with two for loop
+n =int(input())
+for i in range(2*n-1):
+  for j in range(n):
+    if i>=j and i+j<=2*n-2:
+      print("*",end=" ")
+  print()
+
+
 '''
 *
 * *
