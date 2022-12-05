05970b4dd9a2c3eaa8f34761fc96d72679989316
Dinesh1866 <dineshkumarsrds1999@gmail.com>
2022-12-05 20:57:39 +0530

    added pytest

--- Pytesting/Py1/p1test.py
+++ Pytesting/Py1/p1test.py
@@ -4,3 +4,7 @@ from p1 import prime
 class TestP1(unittest.TestCase):
     def test_p1(self):
         self.assertEqual(12.9, prime.printprime(10))
+
+
+if __name__ == '__main__':
+    unittest.main()
